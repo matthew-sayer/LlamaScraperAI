5e3248e56468881426b41c5abb9021089eabadfd
xn43 <matthew.sayer@hotmail.co.uk>
2024-10-04 23:31:54 +0100

    Added streamlit for frontend

---
+++ src/Orchestration/app.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import streamlit as st
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
+
+from src.Data import pipeline as datapl
+from src.AI import ConversationalAI as ai
+
+st.title("WebQnA Conversational AI Tool")
+st.write("Select a URL to ingest data from, and then begin your Question and Answer session.")
+
+
+@st.cache_data
+def load_data(ingestionPath):
+    data = datapl.pipeline(ingestionPath)
+    return data
+
+
+def main():
+
+    url = st.text_input("Enter URL to scrape data from")
+    if st.button("Scrape Data from URL"):
+        st.session_state['data'] = load_data(url)
+        st.session_state['conversation'] = ai.ConversationalAI(st.session_state['data'])
+        st.session_state['url'] = url
+        st.write("Data scraped successfully from source: ", url)
+
+    if 'url' in st.session_state:
+        st.write("Your data is ready for use, you may begin asking questions when you are ready.")
+    
+    if 'conversation' in st.session_state:
+        question = st.text_input("Enter your question here: ")
+        # aadd enter ky to submit question
+        if st.button("Ask Question"):
+            if question:
+                response = st.session_state['conversation'].generateResponse(question)
+                st.write("Question: ", question)
+                st.write("Answer: ", response)
+            else:
+                st.write("Please enter a question before submitting.")
+    if st.button("Clear Data"):
+        st.session_state.clear()
+        st.write("Data cleared successfully. Please enter a new URL to scrape data from.")
+
+
+if __name__ == "__main__":
+    main() 
